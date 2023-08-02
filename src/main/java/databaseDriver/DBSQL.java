ea1f7c92e53120cf95149ad22e0db15cd15b0d40
Leonos8 <covic416@gmail.com>
2023-08-02 16:52:02 -0400

    Adding more for sql

--- src/main/java/databaseDriver/DBSQL.java
+++ src/main/java/databaseDriver/DBSQL.java
@@ -1,32 +1,30 @@
 package databaseDriver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class DBSQL 
 {
+	Connection connection;
+	
 	public static void main(String[] args)
 	{
-		System.out.println("HI");
-		try {
-			DBSQL sql=new DBSQL();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		DBSQL sql=new DBSQL();
 	}
 	
-	public DBSQL() throws SQLException
+	public DBSQL()
 	{
-		String sqlUname="root";
-		String sqlPword="root";
+		String host=null;
+		Integer port=3306;
+		String database="Accounts";
+		String sqlUID="root";
+		String sqlPID="root";
 		
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-		}catch(ClassNotFoundException ex)
-		{
-			ex.printStackTrace();
-		}
+		connection=createConnection(host, port, database, sqlUID, 
+				sqlPID);
 	}
 	
 	public void close()
@@ -37,6 +35,62 @@ public class DBSQL
 	public void containsEntry()
 	{
 		
+	}
+	
+	public Connection createConnection(String host, Integer port, 
+			String database, String sqlUID, String sqlPID)
+	{
+		Connection dbConnection=null;
+		
+		String connectionString="jdbc:mysql:";
+		
+		if(host==null)
+		{
+			connectionString+="//localhost";
+		}
+		else
+		{
+			connectionString+="//"+host;
+		}
+		
+		if(port!=null)
+		{
+			connectionString+=":"+String.valueOf(port);
+		}
+		
+		if(database!=null)
+		{
+			connectionString+="/"+database;
+		}
+		
+		//In case connection uses unicode. Unlikely, but no harm
+		connectionString+="?autoReconnect=true&useUnicode=true&"
+				+ "characterEncoding=UTF8";
+		
+		//Load the driver
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		}catch(ClassNotFoundException ex)
+        {
+        	ex.printStackTrace();
+        } catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			dbConnection=DriverManager.getConnection
+					(connectionString, sqlUID, sqlPID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dbConnection;
 	}
 	
 	//Wont delete an entry, but will make an entry invalid
--- src/main/webapp/login/CreateAccountPage.jsp
+++ src/main/webapp/login/CreateAccountPage.jsp
@@ -66,14 +66,7 @@
   			<div class="container">
     			<label for="uname"><b>Username</b></label>
     			<input id="uname" type="text" placeholder="Enter Username" name="uname" required>
-				<% 
-				   //String uname=
-				  //for(java.util.Enumeration<String> nnn=request.getParameterNames(); nnn.hasMoreElements(); )
-				  //{
-					//  System.out.println("XXX="+nnn.nextElement());
-				  //}
-				  //userHandler.UserHandler.createRequested();
-				%>
+    			
     			<label for="psw"><b>Password</b></label>
     			<input type="password" placeholder="Enter Password" name="psw" required>
         		
